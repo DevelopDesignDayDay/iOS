fc4c30752524c6c9f2048c699399970fa235f07b
godpp <taylored@naver.com>
2019-03-06 18:52:43 +0900

    Add : PopUpView Setting

--- DDD/View/Attend/PopUpView.swift
+++ DDD/View/Attend/PopUpView.swift
@@ -10,12 +10,55 @@ import UIKit
 
 class PopUpView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBAction func acceptButton(_ sender: Any) {
+        self.removeAnimate()
     }
-    */
-
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+        roundCorners(layer: backgroundView.layer, radius: 16)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+        roundCorners(layer: backgroundView.layer, radius: 16)
+    }
+    
+    private func commonInit() {
+        guard let view = Bundle.main.loadNibNamed(
+            "PopUpView",
+            owner: self,
+            options: nil)?.first as? UIView
+            else { return }
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
+    
+    func setupPopUpView(by userType: Int, _ isSuccess: Bool, _ message: String) {
+        switch UserType(userType: userType) {
+        case .admin:
+            backgroundView.backgroundColor = UIColor.dddRed
+        case .general:
+            descriptionLabel.text = message
+            imageView.image = isSuccess ? #imageLiteral(resourceName: "popupCheckIcon") : #imageLiteral(resourceName: "popupExclamationMarkIcon")
+            backgroundView.backgroundColor = UIColor.dddGray
+        }
+    }
+    
+    private func removeAnimate() {
+        UIView.animate(withDuration: 0.25, animations: {
+            self.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+            self.alpha = 0.0
+        }) { (finished) in
+            if finished {
+                self.removeFromSuperview()
+            }
+        }
+    }
 }
